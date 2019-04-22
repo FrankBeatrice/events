577aa51669b130c6f8af951cc82a45bf3e93b54d
FrankBeatrice <Frankie@Franks-MacBook-Pro.local>
2019-04-22 17:13:11 -0400

    renamed:    content/sample-posts/2018-10-19/index.md -> content/_posts/2018-10-19/index.md         modified:   data/SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  blogPostDir: "sample-posts", // The name of directory that contains your posts.
-  blogAuthorDir: "sample-authors", // The name of directory that contains your 'authors' folder.
+  blogPostDir: "_posts", // The name of directory that contains your posts.
+  blogAuthorDir: "_authors", // The name of directory that contains your 'authors' folder.
   blogAuthorId: "casper", // The default and fallback author ID used for blog posts without a defined author.
-  siteTitle: "Gatsby v2 Casper Starter", // Site title.
-  siteTitleAlt: "GatsbyJS Casper Theme Starter", // Alternative site title for SEO.
+  siteTitle: "Delta Flora Presents:", // Site title.
+  siteTitleAlt: "Delta Flora", // Alternative site title for SEO.
   siteLogo:
     "https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
   siteUrl: "https://haysclark.github.io", // Domain of your website without pathPrefix.
